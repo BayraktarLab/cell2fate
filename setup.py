a6b409e6b89b40358deb93334df3e978388a6d89
Alexander Aivazidis <aa16@sanger.ac.uk>
2022-05-04 19:06:59 +0100

    changes to setup.py file to hopefully find and install required packages automatically

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python
-
-# This is a shim to hopefully allow Github to detect the package, build is done with poetry
-
 import setuptools
 
 if __name__ == "__main__":
-    setuptools.setup(name="cell2fate")
-    
-packages=find_packages(include=['sample', 'sample.*']),
+    setuptools.setup()
